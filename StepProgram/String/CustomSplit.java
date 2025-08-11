5b99e946b4104154199e39b0df55bc611eac132c
Ashik A <ashikanparasu@gmail.com>
2025-08-11 09:03:45 +0530

    Update CustomSplit.java

--- StepProgram/String/CustomSplit.java
+++ StepProgram/String/CustomSplit.java
@@ -1,1 +1,84 @@
+import java.util.Scanner;
+
+public class CustomSplit {
+
+    public static int getLength(String str) {
+        int count = 0;
+        try {
+            while (true) {
+                str.charAt(count);
+                count++;
+            }
+        } catch (Exception e) {
+        }
+        return count;
+    }
+
+    public static String[] customSplit(String str) {
+        int len = getLength(str);
+        int wordCount = 0;
+        boolean inWord = false;
+        for (int i = 0; i < len; i++) {
+            if (str.charAt(i) != ' ' && !inWord) {
+                wordCount++;
+                inWord = true;
+            } else if (str.charAt(i) == ' ') {
+                inWord = false;
+            }
+        }
+
+        String[] words = new String[wordCount];
+        int wordIndex = 0;
+        StringBuilder currentWord = new StringBuilder();
+
+        for (int i = 0; i < len; i++) {
+            char ch = str.charAt(i);
+            if (ch != ' ') {
+                currentWord.append(ch);
+            } else if (currentWord.length() > 0) {
+                words[wordIndex++] = currentWord.toString();
+                currentWord.setLength(0);
+            }
+        }
+
+        if (currentWord.length() > 0) {
+            words[wordIndex] = currentWord.toString();
+        }
+
+        return words;
+    }
+
+    public static boolean compareArrays(String[] arr1, String[] arr2) {
+        if (arr1.length != arr2.length) return false;
+        for (int i = 0; i < arr1.length; i++) {
+            if (!arr1[i].equals(arr2[i])) return false;
+        }
+        return true;
+    }
+
+    public static void displayArray(String[] arr) {
+        System.out.print("[");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print("\"" + arr[i] + "\"");
+            if (i != arr.length - 1) System.out.print(", ");
+        }
+        System.out.println("]");
+    }
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a line of text:");
+        String input = scanner.nextLine();
+
+    
+        String[] customSplit = customSplit(input);
+
+
+
+        System.out.println("\nCustom split() result:");
+        displayArray(customSplit);
+
+     
+    }
+}
 
